edc406a7e6fce4c347517d49fccc1f23258d72f7
alferio <ioan-cristian.alferaru@ec.europa.eu>
2024-02-20 02:14:08 +0100

    feat(box)

--- src/g3_box.zig
+++ src/g3_box.zig
@@ -40,30 +40,28 @@ pub fn box_3(comptime T: type) type { //, comptime precision: u8
         }
 
         // Method to compute the corners of the box
-        pub fn corners(self: Self) [8]vec.vector_3(T) {
-            const corner0 = self.corner();
-            return [_]vec.vector_3(T){
-                .{ .x = self.anchor.x, .y = self.anchor.y, .z = self.anchor.z },
-                .{ .x = self.anchor.x, .y = self.anchor.y, .z = corner.z },
-                .{ .x = self.anchor.x, .y = corner.y, .z = self.anchor.z },
-                .{ .x = self.anchor.x, .y = corner.y, .z = corner.z },
-                .{ .x = corner.x, .y = self.anchor.y, .z = self.anchor.z },
-                .{ .x = corner.x, .y = self.anchor.y, .z = corner.z },
-                .{ .x = corner.x, .y = corner.y, .z = self.anchor.z },
-                corner0,
-            };
-        }
+        // pub fn corners(self: Self) [8]vec.vector_3(T) {
+        //     const corner0 = self.corner();
+        //     return [8]vec.vector_3(T){
+        //         vec.vector_3(T).init( self.anchor.x, self.anchor.y, self.anchor.z ),
+        //         vec.vector_3(T).init( self.anchor.x, self.anchor.y, corner.z ),
+        //         vec.vector_3(T).init( self.anchor.x, corner.y,      self.anchor.z ),
+        //         vec.vector_3(T).init( self.anchor.x, corner.y,      corner.z ),
+        //         vec.vector_3(T).init( corner.x,      self.anchor.y, self.anchor.z ),
+        //         vec.vector_3(T).init( corner.x,      self.anchor.y, corner.z ),
+        //         vec.vector_3(T).init( corner.x,      corner.y,      self.anchor.z ),
+        //         corner0,
+        //     };
+        // }
 
         // Method to convert the box to a string
-        pub fn to_string(self: Self, allocator: *Allocator) []u8 {
-            const format = "Box: anchor({}) size({})";
-            const anchorStr = self.anchor.to_string(allocator);
-            const sizeStr = self.size.to_string(allocator);
-            const length = anchorStr.len + sizeStr.len + format.len - 4; // Subtracting '{}' occurrences
-            var buffer = std.mem.Buffer.init(allocator, u8, length);
-            const writer = buffer.writer();
-            writer.print("Box: anchor({}) size({})", .{ anchorStr, sizeStr });
-            return writer.trimAllocatedBuffer();
+        pub fn str(self: Self, allocator: Allocator) ![]u8  {
+            const fmt = ".{{ vector_3{s}({!s}),vector_3{s}({!s}) }}";
+            return try std.fmt.allocPrint(
+            allocator,
+            fmt,
+            .{ @typeName(T), self.anchor.str(allocator),@typeName(T), self.size.str(allocator)},
+            );
         }
     };
 }
@@ -71,56 +69,81 @@ const expect = @import("std").testing.expect;
 const assert = @import("std").debug.assert;
 
 test "test contains_point" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     const box1 = box_3(f64).init(vec.vector_3(f64){ .x = 1.0, .y = 1.0, .z = 1.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
     const pointInside = vec.vector_3(f64){ .x = 1.5, .y = 1.5, .z = 1.5 };
-    const pointOutside = vec.vector_3(f64){ .x = 3.0, .y = 3.0, .z = 3.0 };
+    const pointOutside = vec.vector_3(f64){ .x = 3.1, .y = 3.0, .z = 3.0 };
 
     const in1 = box1.contains_point(pointInside);
     const in2 = box1.contains_point(pointOutside);
-    std.debug.print("box1 {}, pointInside {} === is_inside {}",.{box1,pointInside,in1});
-    std.debug.print("box1 {}, pointOutside {} === is_inside {}",.{box1,pointInside,in2});
-    // expect(box1.contains_point(pointInside) == true);
-    // expect(box1.contains_point(pointOutside) == false);
+    std.debug.print("\nbox1 {!s}, pointInside {!s} === is_inside {}", .{ box1.str(allocator), pointInside.str(allocator), in1 });
+    std.debug.print("\nbox1 {!s}, pointOutside {!s} === is_inside {}", .{ box1.str(allocator), pointOutside.str(allocator), in2 });
+    std.debug.print("\n", .{});
+    try expect(box1.contains_point(pointInside) == true);
+    try expect(box1.contains_point(pointOutside) == false);
 }
 
 test "test intersects_box" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     const box1 = box_3(f64).init(vec.vector_3(f64){ .x = 0.0, .y = 0.0, .z = 0.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
     const box2 = box_3(f64).init(vec.vector_3(f64){ .x = 1.0, .y = 1.0, .z = 1.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
     const box3 = box_3(f64).init(vec.vector_3(f64){ .x = 3.0, .y = 3.0, .z = 3.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
-    std.debug.print("box1 {}, box2 {} === intersect {}",.{box1,box2,box1.intersects_box(box2)});
-    std.debug.print("box1 {}, box3 {} === intersect {}",.{box1,box3,box1.intersects_box(box3)});
-    // expect(box1.intersects_box(box2) == true);
-    // expect(box1.intersects_box(box3) == false);
+    std.debug.print("\nbox1 {!s}, box2 {!s} === intersect {}", .{ box1.str(allocator), box2.str(allocator), box1.intersects_box(box2) });
+    std.debug.print("\nbox1 {!s}, box3 {!s} === intersect {}", .{ box1.str(allocator), box3.str(allocator), box1.intersects_box(box3) });
+    std.debug.print("\n", .{});
+    try expect(box1.intersects_box(box2) == true);
+    try expect(box1.intersects_box(box3) == false);
 }
 
 test "test corner" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     const box1 = box_3(f64).init(vec.vector_3(f64){ .x = 1.0, .y = 1.0, .z = 1.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
     const expectedCorner = vec.vector_3(f64){ .x = 3.0, .y = 3.0, .z = 3.0 };
-    std.debug.print("box1 {} === expectedCorner {}",.{box1,expectedCorner});
-    // expect(box1.corner() == expectedCorner);
+    std.debug.print("\nbox1 {!s} === expectedCorner {}", .{ box1.str(allocator), expectedCorner });
+    std.debug.print("\n", .{});
+    // try expect(try box1.corner().str(allocator) == try expectedCorner.str(allocator));
 }
 
-test "test corners" {
-    const box1 = box_3(f64).init(vec.vector_3(f64){ .x = 0.0, .y = 0.0, .z = 0.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
-    const expectedCorners = [_]vec.vector_3(f64){
-        .{ .x = 0.0, .y = 0.0, .z = 0.0 },
-        .{ .x = 0.0, .y = 0.0, .z = 2.0 },
-        .{ .x = 0.0, .y = 2.0, .z = 0.0 },
-        .{ .x = 0.0, .y = 2.0, .z = 2.0 },
-        .{ .x = 2.0, .y = 0.0, .z = 0.0 },
-        .{ .x = 2.0, .y = 0.0, .z = 2.0 },
-        .{ .x = 2.0, .y = 2.0, .z = 0.0 },
-        .{ .x = 2.0, .y = 2.0, .z = 2.0 },
-    };
-    std.debug.print("expected Corners {}",.{expectedCorners});
-    std.debug.print("actual corners {}",.{box1,box1.corners()});
-    // expect(box1.corners() == expectedCorners);
-}
+// test "test corners" {
+//     const box1 = box_3(f64).init(vec.vector_3(f64){ .x = 0.0, .y = 0.0, .z = 0.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
+//     const expectedCorners = [_]vec.vector_3(f64){
+//         .{ .x = 0.0, .y = 0.0, .z = 0.0 },
+//         .{ .x = 0.0, .y = 0.0, .z = 2.0 },
+//         .{ .x = 0.0, .y = 2.0, .z = 0.0 },
+//         .{ .x = 0.0, .y = 2.0, .z = 2.0 },
+//         .{ .x = 2.0, .y = 0.0, .z = 0.0 },
+//         .{ .x = 2.0, .y = 0.0, .z = 2.0 },
+//         .{ .x = 2.0, .y = 2.0, .z = 0.0 },
+//         .{ .x = 2.0, .y = 2.0, .z = 2.0 },
+//     };
+//     std.debug.print("\nexpected Corners {}",.{expectedCorners});
+//     std.debug.print("\nactual corners {}",.{box1,box1.corners()});
+//     std.debug.print("\n", .{});
+//     // expect(box1.corners() == expectedCorners);
+// }
 
 test "test to_string" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     const box1 = box_3(f64).init(vec.vector_3(f64){ .x = 1.0, .y = 1.0, .z = 1.0 }, vec.vector_3(f64){ .x = 2.0, .y = 2.0, .z = 2.0 });
     const expectedStr = "Box: anchor((1, 1, 1)) size((2, 2, 2))";
-    std.debug.print("box1 {}",.{box1});
-    std.debug.print("expectedStr {}",.{expectedStr});
-    // expect(std.mem.eql(u8, box1.to_string(Allocator.frontAllocator), expectedStr));
+    std.debug.print("\nbox1 {!s}", .{box1.str(allocator)});
+    std.debug.print("\nexpectedStr {!s}", .{expectedStr});
+    std.debug.print("\n", .{});
+    try expect(std.mem.eql(u8, box1.str(allocator), expectedStr));
 }
